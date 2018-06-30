f2cde411873d0ea1370b3393990c153b16065593
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-06-29 23:00:46 -0400

    cursor

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var compScore;
+    var playerScore = 0;
+    var wins = 0;
+    var losses = 0;
+    var gem1;
+    var gem2;
+    var gem3;
+    var gem4;
+
+    $(".gemImage").hover(function() {
+        $(this).css({ opacity: 0.5 })
+    }, function () {
+        $(this).css({ opacity: 1});
+    })
+})
+
+
